3ecce5818838235f4058e98842fc35af310fee20
Zhou Ye <zhouye@stu.pku.edu.cn>
2025-02-13 13:51:31 +0800

    counter_distinct

--- exercises/normal/solution1/src/count_distinct.rs
+++ exercises/normal/solution1/src/count_distinct.rs
@@ -1,3 +1,8 @@
 pub fn new_count_distinct(input_str: &str) -> usize {
-    todo!()
+    // todo!()
+    let mut set = std::collections::HashSet::new();
+    input_str.split(',').for_each(|x| {
+        set.insert(x);
+    });
+    set.len()
 }
